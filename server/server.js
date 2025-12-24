631cbcb1d53fb715301e5cb8402b4e3cd569c23a
Shailendra-221 <shailendrachaudhary221@gmail.com>
2025-12-24 15:41:49 +0530

    fix folder structure

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
-const app = require('./app');
+ const app = require('./app');
 const connectDB = require('./config/db');
 const logger = require('./utils/logger');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
+
 
 // Connect to MongoDB
 connectDB();
 
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
   logger.info(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
 
